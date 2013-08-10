2e30dbe29630375eda5369763cbaaaca6fba692d
TheBronx <salmarina5@gmail.com>
2013-08-10 11:33:13 +0200

    Create shurscript.js

---
+++ shurscript.js
@@ -0,0 +1,20 @@
+// Shur Scripts SA
+// GPLv2 Licensed
+// http://www.gnu.org/licenses/gpl-2.0.html
+//
+// ==UserScript==
+// @name           ShurScript
+// @version        0.00
+// @namespace      http://shurscript.es
+// @author         TheBronx
+// @author         TheBronx
+// @author         TheBronx
+// @author  Buzzy (autoupdate function; see http://userscripts.org/scripts/show/52251 )
+// @include *forocoches.com/foro/*
+// @grant	GM_log
+// @grant 	GM_getValue
+// @grant 	GM_setValue
+// @grant	GM_xmlhttpRequest
+// @grant   GM_registerMenuCommand
+// @history 0.00 first version.
+// ==/UserScript==
