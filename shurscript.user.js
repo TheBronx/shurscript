3308f1e09b65d059acb8acb9dbb8b67c567f7448
xusoo <josepmc90@gmail.com>
2013-08-11 20:03:24 +0200

    Añadir actualizador automático

--- shurscript.user.js
+++ shurscript.user.js
@@ -6,9 +6,7 @@
 // @name			ShurScript
 // @description		Script para ForoCoches
 // @namespace		http://shurscript.es
-// @downloadURL		https://github.com/TheBronx/shurscript/raw/master/shurscript.user.js
-// @updateURL		https://github.com/TheBronx/shurscript/raw/master/shurscript.meta.js
-// @version			0.03
+// @version			0.04
 // @author			TheBronx
 // @author			xusoO
 // @include			*forocoches.com/foro/*
@@ -184,3 +182,94 @@ function setNotificationsCount(count) {
     }
     notificationsDiv.html(count);
 }
+
+
+
+
+
+/* ACTUALIZADOR AUTOMÁTICO */
+// The following code (updated 07/24/13) is released under public domain.
+// Usage guide: https://userscripts.org/guides/45
+
+(function() {
+    var id = 175463,
+      hours = 1,
+      name = typeof GM_info === 'object' ? GM_info.script.name : 'ShurScript',
+      version = typeof GM_info === 'object' ? GM_info.script.version : '0.02',
+      time = new Date().getTime();
+    function call(response, secure) {
+        GM_xmlhttpRequest({
+            method: 'GET',
+            url: 'http'+(secure ? 's' : '')+'://userscripts.org/scripts/source/'+id+'.meta.js',
+            onload: function(xpr) {compare(xpr, response);},
+            onerror: function(xpr) {if (secure) call(response, false);}
+        });
+    }
+    function enable() {
+        GM_registerMenuCommand('Activar actualizaciones automáticas del '+name, function() {
+            GM_setValue('updated_175463', new Date().getTime()+'');
+            call(true, true)
+        });
+    }
+    function compareVersion(r_version, l_version) {
+        var r_parts = r_version.split('.'),
+          l_parts = l_version.split('.'),
+          r_len = r_parts.length,
+          l_len = l_parts.length,
+          r = l = 0;
+        for(var i = 0, len = (r_len > l_len ? r_len : l_len); i < len && r == l; ++i) {
+            r = +(r_parts[i] || '0');
+            l = +(l_parts[i] || '0');
+        }
+        return (r !== l) ? r > l : false;
+    }
+    function compare(xpr,response) {
+        var xversion=/\/\/\s*@version\s+(.+)\s*\n/i.exec(xpr.responseText);
+        var xname=/\/\/\s*@name\s+(.+)\s*\n/i.exec(xpr.responseText);
+        if ( (xversion) && (xname[1] == name) ) {
+            xversion = xversion[1];
+            xname = xname[1];
+        } else {
+            if ( (xpr.responseText.match('the page you requested doesn\'t exist')) || (xname[1] != name) )
+            GM_setValue('updated_175463', 'off');
+            return false;
+        }
+        var updated = compareVersion(xversion, version);
+        if ( updated && confirm('Hay disponible una nueva versión del '+xname+'.\n¿Quieres instalarla?') ) {
+            try {
+                location.href = 'http://userscripts.org/scripts/source/'+id+'.user.js';
+            } catch(e) {}
+        } else if ( xversion && updated ) {
+            if(confirm('¿Quieres desactivar las actualizaciones automáticas de este script?')) {
+                GM_setValue('updated_175463', 'off');
+                enable();
+                alert('Puedes volver a activarlas desde el submenú User Script Commands.');
+            }
+        } else if (response)
+            alert('No hay actualizaciones disponibles del '+name);
+    }
+    function check() {
+        if (GM_getValue('updated_175463', 0) == 'off')
+            enable();
+        else {
+            if (+time > (+GM_getValue('updated_175463', 0) + 1000*60*60*hours)) {
+                GM_setValue('updated_175463', time+'');
+                call(false, true);
+            }
+            GM_registerMenuCommand('Comprobar actualizaciones del '+name, function() {
+                GM_setValue('updated_175463', new Date().getTime()+'');
+                call(true, true);
+            });
+        }
+    }
+    if (typeof GM_xmlhttpRequest !== 'undefined' &&
+        (typeof GM_info === 'object' ? // has a built-in updater?
+         GM_info.scriptWillUpdate === false : true))
+        try {
+            if (unsafeWindow.frameElement === null) check();
+        } catch(e) {
+            check();
+        }
+})();
+
+
