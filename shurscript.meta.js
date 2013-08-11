41401a0778d255c41dd7edc597809ee17db9155f
xusoo <josepmc90@gmail.com>
2013-08-11 17:54:49 +0200

    Create shurscript.meta.js
    Necesario para las actualizaciones automáticas

---
+++ shurscript.meta.js
@@ -0,0 +1,21 @@
+// Shur Scripts SA
+// GPLv2 Licensed
+// http://www.gnu.org/licenses/gpl-2.0.html
+//
+// ==UserScript==
+// @name  		ShurScript
+// @description		Script para ForoCoches
+// @namespace		http://shurscript.es
+// @version			0.02
+// @author			TheBronx
+// @author			xusoO
+// @include			*forocoches.com/foro/*
+// @require			http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// @grant	GM_log
+// @grant	GM_getValue
+// @grant	GM_setValue
+// @grant	GM_xmlhttpRequest
+// @grant	GM_registerMenuCommand
+// @grant	GM_addStyle
+// @history 0.00 first version.
+// ==/UserScript==
