2e547cf9a13b7f9da69947e0af56f83f3d2761be
xusoo <xusoo@users.noreply.github.com>
2013-09-16 19:21:06 +0200

    Actualizadas las rutas de imágenes a las nuevas del CDN

--- modules/BetterPosts.js
+++ modules/BetterPosts.js
@@ -1,5 +1,4 @@
 
-
 function BetterPosts() {
 
 	this.id = arguments.callee.name;
@@ -57,7 +56,7 @@ function BetterPosts() {
 		
 		
 		if ($('#' + editor.editorid + '_cmd_switchmode').length == 0) //Añadimos el boton de cambiar de Editor
-			$('<td><div id="' + editor.editorid + '_cmd_switchmode" class="imagebutton" style="background: none repeat scroll 0% 0% rgb(225, 225, 226); color: rgb(0, 0, 0); padding: 1px; border: medium none;"><img height="20" width="21" alt="Cambiar Modo de Editor" src="http://st.forocoches.com/foro/images/editor/switchmode.gif" title="Cambiar Modo de Editor"></div></td>').insertAfter($('#vB_Editor_QR_cmd_resize_0_99').parent());
+			$('<td><div id="' + editor.editorid + '_cmd_switchmode" class="imagebutton" style="background: none repeat scroll 0% 0% rgb(225, 225, 226); color: rgb(0, 0, 0); padding: 1px; border: medium none;"><img height="20" width="21" alt="Cambiar Modo de Editor" src="http://cdn.forocoches.com/foro/images/editor/switchmode.gif" title="Cambiar Modo de Editor"></div></td>').insertAfter($('#vB_Editor_QR_cmd_resize_0_99').parent());
 		
 		
 	}
@@ -319,23 +318,23 @@ function BetterPosts() {
 		var fieldset = $('<fieldset class="fieldset" style="margin:3px 0px 0px 0px"><legend>Iconos</legend></fieldset>');
 		$("#" + getEditor().editorid).parent().append(fieldset);
 				
-		fieldset.append(createIcon(":roto2:", "http://st.forocoches.com/foro/images/smilies/goofy.gif", 164));
-		fieldset.append(createIcon(":sisi3:", "http://st.forocoches.com/foro/images/smilies/sisi3.gif", 324));
-		fieldset.append(createIcon(":mola:", "http://st.forocoches.com/foro/images/smilies/thumbsup.gif", 48));
-		fieldset.append(createIcon(":cantarin:", "http://st.forocoches.com/foro/images/smilies/Sing.gif", 101));
-		fieldset.append(createIcon(":qmeparto:", "http://st.forocoches.com/foro/images/smilies/meparto.gif", 142));
-		fieldset.append(createIcon(":nusenuse:", "http://st.forocoches.com/foro/images/smilies/nusenuse.gif", 283));
-		fieldset.append(createIcon(":zpalomita", "http://st.forocoches.com/foro/images/smilies/icon_popcorn.gif", 215));
-		fieldset.append(createIcon(":zplatano2", "http://st.forocoches.com/foro/images/smilies/b2.gif", 236));
-		fieldset.append(createIcon(":number1:", "http://st.forocoches.com/foro/images/smilies/number_one.gif", 268));
-		fieldset.append(createIcon(":elrisas:", "http://st.forocoches.com/foro/images/smilies/qmeparto.gif", 76));
-		fieldset.append(createIcon(":gaydude:", "http://st.forocoches.com/foro/images/smilies/gaydude.gif", 264));
-		fieldset.append(createIcon(":sisi1:", "http://st.forocoches.com/foro/images/smilies/sisi1.gif", 299));
-		fieldset.append(createIcon(":babeando:", "http://st.forocoches.com/foro/images/smilies/babeando.gif", 274));
-		fieldset.append(createIcon(":elboinas:", "http://st.forocoches.com/foro/images/smilies/elboinas.gif", 314));
-		fieldset.append(createIcon(":sherlock:", "http://st.forocoches.com/foro/images/smilies/sherlock.gif", 281));
-		fieldset.append(createIcon(":qtedoy:", "http://st.forocoches.com/foro/images/smilies/smiley_1140.gif", 191));
-		fieldset.append(createIcon(":abrazo:", "http://st.forocoches.com/foro/images/smilies/abrazo.gif", 161));
+		fieldset.append(createIcon(":roto2:", "http://cdn.forocoches.com/foro/images/smilies/goofy.gif", 164));
+		fieldset.append(createIcon(":sisi3:", "http://cdn.forocoches.com/foro/images/smilies/sisi3.gif", 324));
+		fieldset.append(createIcon(":mola:", "http://cdn.forocoches.com/foro/images/smilies/thumbsup.gif", 48));
+		fieldset.append(createIcon(":cantarin:", "http://cdn.forocoches.com/foro/images/smilies/Sing.gif", 101));
+		fieldset.append(createIcon(":qmeparto:", "http://cdn.forocoches.com/foro/images/smilies/meparto.gif", 142));
+		fieldset.append(createIcon(":nusenuse:", "http://cdn.forocoches.com/foro/images/smilies/nusenuse.gif", 283));
+		fieldset.append(createIcon(":zpalomita", "http://cdn.forocoches.com/foro/images/smilies/icon_popcorn.gif", 215));
+		fieldset.append(createIcon(":zplatano2", "http://cdn.forocoches.com/foro/images/smilies/b2.gif", 236));
+		fieldset.append(createIcon(":number1:", "http://cdn.forocoches.com/foro/images/smilies/number_one.gif", 268));
+		fieldset.append(createIcon(":elrisas:", "http://cdn.forocoches.com/foro/images/smilies/qmeparto.gif", 76));
+		fieldset.append(createIcon(":gaydude:", "http://cdn.forocoches.com/foro/images/smilies/gaydude.gif", 264));
+		fieldset.append(createIcon(":sisi1:", "http://cdn.forocoches.com/foro/images/smilies/sisi1.gif", 299));
+		fieldset.append(createIcon(":babeando:", "http://cdn.forocoches.com/foro/images/smilies/babeando.gif", 274));
+		fieldset.append(createIcon(":elboinas:", "http://cdn.forocoches.com/foro/images/smilies/elboinas.gif", 314));
+		fieldset.append(createIcon(":sherlock:", "http://cdn.forocoches.com/foro/images/smilies/sherlock.gif", 281));
+		fieldset.append(createIcon(":qtedoy:", "http://cdn.forocoches.com/foro/images/smilies/smiley_1140.gif", 191));
+		fieldset.append(createIcon(":abrazo:", "http://cdn.forocoches.com/foro/images/smilies/abrazo.gif", 161));
 		var more = $("<a href='#qrform'>Más...</a>");
 		more.click(function() {
 			getEditor().open_smilie_window(785, 500);
@@ -358,24 +357,24 @@ function BetterPosts() {
 		buttons.push(createButton("justifyleft", "Alinear a la Izquierda"));
 		buttons.push(createButton("justifycenter", "Alinear al Centro"));
 		buttons.push(createButton("justifyright", "Alinear a la Derecha"));
-		buttons.push('<td><img width="6" height="20" alt="" src="http://st.forocoches.com/foro/images/editor/separator.gif"></td>');
+		buttons.push('<td><img width="6" height="20" alt="" src="http://cdn.forocoches.com/foro/images/editor/separator.gif"></td>');
 		buttons.push(createButton("insertorderedlist", "Lista Ordenada"));
 		buttons.push(createButton("insertunorderedlist", "Lista sin Ordenar"));
-		buttons.push('<td><img width="6" height="20" alt="" src="http://st.forocoches.com/foro/images/editor/separator.gif"></td>');
+		buttons.push('<td><img width="6" height="20" alt="" src="http://cdn.forocoches.com/foro/images/editor/separator.gif"></td>');
 		buttons.push(createButton("undo", "Deshacer"));
 		buttons.push(createButton("redo", "Rehacer"));
-		buttons.push('<td><img width="6" height="20" alt="" src="http://st.forocoches.com/foro/images/editor/separator.gif"></td>');
+		buttons.push('<td><img width="6" height="20" alt="" src="http://cdn.forocoches.com/foro/images/editor/separator.gif"></td>');
 		buttons.push(createButton("wrap0_code", "Envolver Etiquetas [CODE]", 'code'));
 		buttons.push(createButton("wrap0_html", "Envolver Etiquetas [HTML]", 'html'));
 		buttons.push(createButton("wrap0_php", "Envolver Etiquetas [PHP]", 'php'));
-		buttons.push('<td><img width="6" height="20" alt="" src="http://st.forocoches.com/foro/images/editor/separator.gif"></td>');
+		buttons.push('<td><img width="6" height="20" alt="" src="http://cdn.forocoches.com/foro/images/editor/separator.gif"></td>');
 	
 		toolbar.after(buttons);
 	}
 	
 	function createButton(action, text, icon) {
 		var img = icon ? icon : action;
-		var button = $('<div id="vB_Editor_001_cmd_' + action + '" class="imagebutton" style="background: none repeat scroll 0% 0% rgb(225, 225, 226); color: rgb(0, 0, 0); padding: 1px; border: medium none;"><img width="21" height="20" alt="' + text + '" src="http://st.forocoches.com/foro/images/editor/' + img + '.gif" title="' + text + '"></div>')[0];
+		var button = $('<div id="vB_Editor_001_cmd_' + action + '" class="imagebutton" style="background: none repeat scroll 0% 0% rgb(225, 225, 226); color: rgb(0, 0, 0); padding: 1px; border: medium none;"><img width="21" height="20" alt="' + text + '" src="http://cdn.forocoches.com/foro/images/editor/' + img + '.gif" title="' + text + '"></div>')[0];
 		button.editorid = getEditor().editorid;
 		button.cmd = action;
 		button.onclick = button.onmousedown = button.onmouseover = button.onmouseout = genericHandler;
@@ -437,7 +436,7 @@ function BetterPosts() {
 		/*
 var fontsHandler = function (A){A=unsafeWindow.do_an_e(A);if(A.type=="click"){this._onclick(A);unsafeWindow.vB_Editor[getEditor().editorid].menu_context(this,"mouseover")}else{unsafeWindow.vB_Editor[getEditor().editorid].menu_context(this,A.type)}};
 		var fontnameMenu = $('<div id="' + getEditor().editorid + '_popup_fontname_menu" class="vbmenu_popup" style="cursor: default; padding: 3px; width: 200px; height: 250px; overflow: auto; position: absolute; z-index: 50; clip: rect(auto, auto, auto, auto); left: 703.317px; top: 720.433px;"></div>');
-		var fontnameButton = $('<div title="Fuentes" id="' + getEditor().editorid + '_popup_fontname" class="imagebutton" style="background: none repeat scroll 0% 0% rgb(225, 225, 226); color: rgb(0, 0, 0); padding: 1px; border: medium none;"><table cellspacing="0" cellpadding="0" border="0"><tbody><tr><td class="popup_feedback" style="border-right: 1px solid rgb(255, 255, 255);"><div style="width:91px" id="vB_Editor_001_font_out">Fuentes</div><div id="vB_Editor_001_fontoption_Arial" style="width: 91px; display: none;">Arial</div><div id="vB_Editor_001_fontoption_Arial Black" style="width: 91px; display: none;">Arial Black</div><div id="vB_Editor_001_fontoption_Arial Narrow" style="width: 91px; display: none;">Arial Narrow</div><div id="vB_Editor_001_fontoption_Book Antiqua" style="width: 91px; display: none;">Book Antiqua</div><div id="vB_Editor_001_fontoption_Century Gothic" style="width: 91px; display: none;">Century Gothic</div><div id="vB_Editor_001_fontoption_Comic Sans MS" style="width: 91px; display: none;">Comic Sans MS</div><div id="vB_Editor_001_fontoption_Courier New" style="width: 91px; display: none;">Courier New</div><div id="vB_Editor_001_fontoption_Fixedsys" style="width: 91px; display: none;">Fixedsys</div><div id="vB_Editor_001_fontoption_Franklin Gothic Medium" style="width: 91px; display: none;">Franklin Gothic Medium</div><div id="vB_Editor_001_fontoption_Garamond" style="width: 91px; display: none;">Garamond</div><div id="vB_Editor_001_fontoption_Georgia" style="width: 91px; display: none;">Georgia</div><div id="vB_Editor_001_fontoption_Impact" style="width: 91px; display: none;">Impact</div><div id="vB_Editor_001_fontoption_Lucida Console" style="width: 91px; display: none;">Lucida Console</div><div id="vB_Editor_001_fontoption_Lucida Sans Unicode" style="width: 91px; display: none;">Lucida Sans Unicode</div><div id="vB_Editor_001_fontoption_Microsoft Sans Serif" style="width: 91px; display: none;">Microsoft Sans Serif</div><div id="vB_Editor_001_fontoption_Palatino Linotype" style="width: 91px; display: none;">Palatino Linotype</div><div id="vB_Editor_001_fontoption_System" style="width: 91px; display: none;">System</div><div id="vB_Editor_001_fontoption_Tahoma" style="width: 91px; display: none;">Tahoma</div><div id="vB_Editor_001_fontoption_Times New Roman" style="width: 91px; display: none;">Times New Roman</div><div id="vB_Editor_001_fontoption_Trebuchet MS" style="width: 91px; display: none;">Trebuchet MS</div><div id="vB_Editor_001_fontoption_Verdana" style="width: 91px; display: none;">Verdana</div></td><td class="popup_pickbutton" style="border-color: rgb(255, 255, 255);"><img width="11" height="16" alt="" src="http://st.forocoches.com/foro/images/editor/menupop.gif"></td></tr></tbody></table></div>');
+		var fontnameButton = $('<div title="Fuentes" id="' + getEditor().editorid + '_popup_fontname" class="imagebutton" style="background: none repeat scroll 0% 0% rgb(225, 225, 226); color: rgb(0, 0, 0); padding: 1px; border: medium none;"><table cellspacing="0" cellpadding="0" border="0"><tbody><tr><td class="popup_feedback" style="border-right: 1px solid rgb(255, 255, 255);"><div style="width:91px" id="vB_Editor_001_font_out">Fuentes</div><div id="vB_Editor_001_fontoption_Arial" style="width: 91px; display: none;">Arial</div><div id="vB_Editor_001_fontoption_Arial Black" style="width: 91px; display: none;">Arial Black</div><div id="vB_Editor_001_fontoption_Arial Narrow" style="width: 91px; display: none;">Arial Narrow</div><div id="vB_Editor_001_fontoption_Book Antiqua" style="width: 91px; display: none;">Book Antiqua</div><div id="vB_Editor_001_fontoption_Century Gothic" style="width: 91px; display: none;">Century Gothic</div><div id="vB_Editor_001_fontoption_Comic Sans MS" style="width: 91px; display: none;">Comic Sans MS</div><div id="vB_Editor_001_fontoption_Courier New" style="width: 91px; display: none;">Courier New</div><div id="vB_Editor_001_fontoption_Fixedsys" style="width: 91px; display: none;">Fixedsys</div><div id="vB_Editor_001_fontoption_Franklin Gothic Medium" style="width: 91px; display: none;">Franklin Gothic Medium</div><div id="vB_Editor_001_fontoption_Garamond" style="width: 91px; display: none;">Garamond</div><div id="vB_Editor_001_fontoption_Georgia" style="width: 91px; display: none;">Georgia</div><div id="vB_Editor_001_fontoption_Impact" style="width: 91px; display: none;">Impact</div><div id="vB_Editor_001_fontoption_Lucida Console" style="width: 91px; display: none;">Lucida Console</div><div id="vB_Editor_001_fontoption_Lucida Sans Unicode" style="width: 91px; display: none;">Lucida Sans Unicode</div><div id="vB_Editor_001_fontoption_Microsoft Sans Serif" style="width: 91px; display: none;">Microsoft Sans Serif</div><div id="vB_Editor_001_fontoption_Palatino Linotype" style="width: 91px; display: none;">Palatino Linotype</div><div id="vB_Editor_001_fontoption_System" style="width: 91px; display: none;">System</div><div id="vB_Editor_001_fontoption_Tahoma" style="width: 91px; display: none;">Tahoma</div><div id="vB_Editor_001_fontoption_Times New Roman" style="width: 91px; display: none;">Times New Roman</div><div id="vB_Editor_001_fontoption_Trebuchet MS" style="width: 91px; display: none;">Trebuchet MS</div><div id="vB_Editor_001_fontoption_Verdana" style="width: 91px; display: none;">Verdana</div></td><td class="popup_pickbutton" style="border-color: rgb(255, 255, 255);"><img width="11" height="16" alt="" src="http://cdn.forocoches.com/foro/images/editor/menupop.gif"></td></tr></tbody></table></div>');
 		fontnameButton[0].cmd = 'fontname';
 		fontnameButton[0].editorid = getEditor().editorid;
 		
@@ -513,7 +512,7 @@ function quickPreview() {
 		if (username) {
 			quote += '<div>Originalmente Escrito por <b>' + username + '</b>';
 			if (post) {
-				quote += '<a rel="nofollow" href="showthread.php?p=' + post + '#post' + post + '"><img border="0" alt="Ver Mensaje" src="http://st.forocoches.com/foro/images/buttons/viewpost.gif" class="inlineimg" title="Ver Mensaje"></a>';
+				quote += '<a rel="nofollow" href="showthread.php?p=' + post + '#post' + post + '"><img border="0" alt="Ver Mensaje" src="http://cdn.forocoches.com/foro/images/buttons/viewpost.gif" class="inlineimg" title="Ver Mensaje"></a>';
 			}
 			
 			quote += '</div><div style="font-style:italic">' + text + '</div>';
