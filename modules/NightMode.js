2c8478af35eaed8b5ca398c4ced1ade1eb0fb155
ikaros45 <bgusach@gmail.com>
2013-10-02 18:55:11 +0200

    Corregidos unos detalles

--- modules/NightMode.js
+++ modules/NightMode.js
@@ -1,19 +1,14 @@
 function NightMode() {
 
-    this.id = arguments.callee.name; //ModuleTemplate
+    this.id = arguments.callee.name;
     this.name = 'NightMode';
     this.author = 'ikaros45 / Juno';
-    this.version = '0.1';
+    this.version = '0.11';
     this.description = 'Cambia tema a colores oscuros';
-    this.enabledByDefault = false; //Define si el modulo vendrá activado por defecto o no
+    this.enabledByDefault = true; //Define si el modulo vendrá activado por defecto o no
     this.worksInFrontPage = true; // Modulo carga en portada
 
     var helper = new ScriptHelper(this.id);
-
-    /* Define una condición a la carga del módulo. Si no se quiere condición, eliminar este metodo o devolver true. */
-    this.shouldLoad = function() {
-        return true;
-    };
 
     /* Método obligatorio y punto de entrada a la lógica del módulo */
     this.load = function() {
