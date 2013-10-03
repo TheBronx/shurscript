f36093e3c1b3c84d8c26f08eb17e1203fc62a5c8
ikaros45 <bgusach@gmail.com>
2013-10-03 22:39:57 +0200

    Restructurando modulos2

--- core.js
+++ core.js
@@ -1,10 +1,26 @@
-var SHURSCRIPT = (function ($) {
+var SHURSCRIPT = (function ($, GM, undefined) {
     'use strict';
 
     var self = {};
+/*
+    self.name = 'core';
+    self.helper = SHURSCRIPT.getHelper(self.name);
 
+    self.GM = GM;
     self.modules = {};
 
-    return self;
+    self.initialize = function () {
 
-})(jQuery);
+        self.env = {
+            page: location.pathname.replace("/foro","")
+        };
+
+    };
+
+    self.loadModules = function () {
+
+    };
+
+*/
+    return self;
+})(jQuery, GREASEMONKEY);
