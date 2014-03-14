4ef1756275ac8eccf955d2d2bae4523c0e37282e
Electrosa <ivanperez2@gmail.com>
2014-03-14 23:20:35 +0100

    Añado opciones y mejoro los estilos.
    Añado opciones al módulo HighlightOP.
    Mejoro los estilos usados para resaltar los posts y las citas.
    Botón para buscar todos los posts del OP en el hilo.

--- modules/HighlightOP.js
+++ modules/HighlightOP.js
@@ -10,14 +10,25 @@ function HighlightOP() {
     var helper = new ScriptHelper(this.id);
     
     var currentThread, currentPage;
+    var optionMyPosts, optionQuotes;// preferences
     
     this.shouldLoad = function() {
         return page === "/showthread.php";
     }
     
+    this.getPreferences = function() {
+        return [
+            new BooleanPreference("HIGHLIGHT_MY_POSTS", true, "Resaltar mis propios posts."),
+            new BooleanPreference("HIGHLIGHT_QUOTES", true, "Resaltar citas.")
+        ];
+    }
+    
     this.load = function() {
         currentThread = getCurrentThread();
         currentPage = getCurrentPage();
+        
+        optionMyPosts = helper.getValue("HIGHLIGHT_MY_POSTS", false);
+        optionQuotes = helper.getValue("HIGHLIGHT_QUOTES", true);
         
         // If not in first page, we must load it to get OP's name.
         if (currentPage === 1) {
@@ -65,41 +76,53 @@ function HighlightOP() {
             sessionStorage["op_" + currentThread] = op;
         }
         
-        // Add CSS rule
-        GM_addStyle(".op_post, .op_quote { background-color: #B1D4D7; }");
-        GM_addStyle(".my_post, .my_quote { border: 3px solid blue !important; border-radius: 5px; }");
+        // Add CSS rules
+        GM_addStyle(".op_post, .op_quote { border-left: 8px solid #D90602 !important; } .op_post td.alt2 { width: 167px; }");
+        GM_addStyle(".my_post, .my_quote { border-left: 8px solid #615662 !important; } .op_post td.alt2 { width: 167px; }");
         
         // Highlighted posts have "op_post" class
         for (var i = 0, n = users.length; i < n; i++) {
             var currentUser = users[i].innerHTML;
             
             if (currentUser === op) {
-                users[i].parentNode.parentNode.classList.add("op_post");
+                users[i].parentNode.parentNode.parentNode.parentNode.parentNode.classList.add("op_post");
             }
             
-            if (currentUser === username) {
+            if (optionMyPosts && currentUser === username) {
                 users[i].parentNode.parentNode.parentNode.parentNode.parentNode.classList.add("my_post");
             }
         }
         
         // Highlighted quotes have "op_quote" class
-        var quotes = document.getElementsByClassName("alt2");
-        
-        for (var i = 0, n = quotes.length; i < n; i++) {
-            var elem = quotes[i].getElementsByTagName("B");
+        if (optionQuotes) {
+            var quotes = document.getElementsByClassName("alt2");
             
-            if (elem && elem.length > 0) {
-                var quotedUser = elem[0].innerHTML;
-            
-                if (quotedUser === op) {
-                    quotes[i].classList.add("op_quote");
-                }
+            for (var i = 0, n = quotes.length; i < n; i++) {
+                var elem = quotes[i].getElementsByTagName("B");
                 
-                if (quotedUser === username) {
-                    quotes[i].classList.add("my_quote");
+                if (elem && elem.length > 0) {
+                    var quotedUser = elem[0].innerHTML;
+                
+                    if (quotedUser === op) {
+                        quotes[i].classList.add("op_quote");
+                    }
+                    
+                    if (optionMyPosts && quotedUser === username) {
+                        quotes[i].classList.add("my_quote");
+                    }
                 }
             }
         }
+        
+        // Add a link to find all OP's posts on this thread.
+        var tdNextNode = document.getElementById("threadtools");
+        var trNode = tdNextNode.parentNode;
+        
+        var newTd = document.createElement("TD");
+        newTd.className = 'vbmenu_control';
+        newTd.innerHTML = '<a href="/foro/search.php?do=process&searchthreadid=' + currentThread + '&searchuser=' + escape(op) + '&exactname=1">Buscar posts del OP</a>';
+        
+        trNode.insertBefore(newTd, tdNextNode);
     }
 
     function getCurrentPage() {
