1a68c0194793f26e608a1e9ef961a95161397cc4
xusoo <Josep@MacBook-de-Josep.local>
2014-03-16 15:32:32 +0100

    Actualizar versión

--- shurscript.user.js
+++ shurscript.user.js
@@ -6,7 +6,7 @@
 // @name            ShurScript
 // @description     Script para ForoCoches
 // @namespace       http://shurscript.es
-// @version         0.10.2-dev
+// @version         0.10.2
 // @author          TheBronx
 // @author          xusoO
 // @author          Fritanga
