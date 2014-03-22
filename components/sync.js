52281f02d9d2c304f4aefd3392bb19382a913b62
TheBronx <salmarina5@gmail.com>
2014-03-22 15:25:09 +0100

    helper.log corregidos en sync.js
    corregida concatenación al hacer log

--- components/sync.js
+++ components/sync.js
@@ -42,7 +42,7 @@
 				data: "",
 				dataType: 'json'
 			}).done(function (data) {
-					sync.helper.log("Server answer:", data);
+					sync.helper.log("Server answer:" + data);
 				});
 		},
 
@@ -54,7 +54,7 @@
 				data: "",
 				dataType: 'json'
 			}).done(function (data) {
-					sync.helper.log("Server answer:", data);
+					sync.helper.log("Server answer:" + data);
 					Cloud.preferences = data;
 					callback();
 				});
@@ -74,7 +74,7 @@
 				data: "",
 				dataType: 'json'
 			}).done(function (data) {
-					sync.helper.log("Server answer:", data);
+					sync.helper.log("Server answer:" + data);
 					Cloud.apiKey = data.apikey;
 					saveApiKey(Cloud.apiKey); //guardamos la API key generada en las suscripciones
 					callback();
