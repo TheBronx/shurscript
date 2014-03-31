b176510903e48319d68666aa7e4f2f526ca1a835
xusoo <josepmc90@gmail.com>
2014-03-31 20:15:56 +0200

    Timeout al mostrar mensaje de error del servidor para evitar falsos positivos al cambiar de página rápidamente cortando la llamada asíncrona

--- core.js
+++ core.js
@@ -345,13 +345,15 @@ var SHURSCRIPT = {
 			core.helper.deleteLocalValue('SERVER_DOWN_ALERT');
 		}).fail(function(error){
 			if (!core.helper.getLocalValue('SERVER_DOWN_ALERT')) {
-				core.helper.showMessageBar({
-					message: "<strong>Oops...</strong> Parece que se ha roto alguna pieza en el servidor de <strong>Shurscript</strong>. Int&eacute;ntalo de nuevo en unos minutos o deja constancia en el <a href='http://shurscript.org/hilo'>hilo oficial</a>.",
-					type: "danger",
-					onClose: function() {
-						core.helper.setLocalValue('SERVER_DOWN_ALERT', true);
-					}
-				});
+				setTimeout(function () { //Timeout para evitar falsos positivos al cambiar de página rápidamente cortando la llamada asíncrona
+					core.helper.showMessageBar({
+						message: "<strong>Oops...</strong> Parece que se ha roto alguna pieza en el servidor de <strong>Shurscript</strong>. Int&eacute;ntalo de nuevo en unos minutos o deja constancia en el <a href='http://shurscript.org/hilo'>hilo oficial</a>.",
+						type: "danger",
+						onClose: function() {
+							core.helper.setLocalValue('SERVER_DOWN_ALERT', true);
+						}
+					});
+				}, 2000);
 			}
 		});
 
