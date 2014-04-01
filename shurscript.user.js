738e83d8a8e391cccd8847232bddcee0d9f39c20
xusoo <josepmc90@gmail.com>
2014-04-01 20:03:39 +0200

    Cambiar versión

--- shurscript.user.js
+++ shurscript.user.js
@@ -6,7 +6,7 @@
 // @name            ShurScript
 // @description     Script para ForoCoches
 // @namespace       http://shurscript.es
-// @version         0.20-exp
+// @version         0.20.1-exp
 // @author          TheBronx
 // @author          xusoO
 // @author          Fritanga
