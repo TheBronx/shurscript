f1ad4f5c76f06dedd2a57f73b66a14eba0058de9
TheBronx <salmarina5@gmail.com>
2014-04-23 17:25:58 +0200

    filterthreads empieza a funcionar con el parser
    las piezas van encajando

--- components/parser.js
+++ components/parser.js
@@ -3,6 +3,10 @@
  * como puedan ser hilos, posts o usuarios
  * OJO: necesita que el componente eventbus haya cargado
  * OJO: el parse empieza cuando se llama a parser.parse() y entonces los eventos se van disparando
+ *
+ * Eventos lanzados:
+ *      + parseThread( Thread )
+ *
  */
 (function ($, SHURSCRIPT, undefined) {
 	'use strict';
@@ -10,33 +14,32 @@
 	var parser = SHURSCRIPT.core.createComponent('parser');
 
 	var Thread = function(element) {
-		var _this = this;
 		this.element = element;
-
-		this.title_td = $(this).find('td[id^="td_threadtitle_"]');
-
-		if (this.title_td.length != 0) {
+		var title_td = element.find('td[id^="td_threadtitle_"]');
+		//comprobamos que el <tr> es un hilo y no una fila vacia (gracias ilitri :manco:)
+		if (title_td.length != 0) {
+			this.title_td = title_td;
 			this.title_link = this.title_td.find('div > a[id^="thread_title_"]').first();
 			this.href = this.title_link.attr('href');
 			this.id = parseInt(/.*showthread\.php\?.*t=([0-9]+).*/.exec(this.href)[1]);
 			this.title = this.title_link.html();
 			this.creator_span = this.title_td.find("div.smallfont > span:last-child");
 			this.author = this.creator_span.text();
-		} else {
-			console.log("This is not a thread, how is that possible??? "+this.element);
+			this.icon_td = this.element.find('#td_threadstatusicon_' + this.id);
 		}
 	};
 
 	parser.parse = function () {
-		if (parser.helper.environment.page == "/") {
+		var page = location.pathname.indexOf("/foro") != -1 ? location.pathname.replace("/foro", "") : "frontpage";
+		if (page == "frontpage") {
 			//si estamos en la portada, parse de hilos
 			//TODO parsePortalThreads()
 
-		} else if (parser.helper.environment.page == "/showthread.php") {
+		} else if (page == "/showthread.php") {
 			//si estamos en un hilo, parse de posts y usuarios
 			//TODO parsePostsAndUsers()
 
-		} else if (parser.helper.environment.page == "/forumdisplay.php" || parser.helper.environment.page == "/search.php") {
+		} else if (page == "/forumdisplay.php" || page == "/search.php") {
 			//si estamos en una seccion o una búsqueda, parse de hilos
 			parseThreads();
 		}
@@ -50,8 +53,10 @@
 		//Recorremos todos los hilos de la lista
 		$('#threadslist tr').each(function (index) {
 			var thread = new Thread($(this));
-			console.log("triggering parseThread for thread id: "+thread.id);
-			SHURSCRIPT.eventbus.trigger('parseThread', thread);
+			if (thread.id!=undefined) {
+				console.log("triggering parseThread for thread id: "+thread.id);
+				SHURSCRIPT.eventbus.trigger('parseThread', thread);
+			}
 		});
 	}
 
