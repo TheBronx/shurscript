e2f827e666a331b5138aaa874b8a3b564a1c1609
Rubén del Campo <yo@rubendelcampo.es>
2014-04-24 09:40:28 +0200

    No habíamos añadido el sufijo -exp

--- shurscript.user.js
+++ shurscript.user.js
@@ -6,7 +6,7 @@
 // @name            ShurScript
 // @description     Script para ForoCoches
 // @namespace       http://shurscript.es
-// @version         0.20.5
+// @version         0.20.5-exp
 // @author          TheBronx
 // @author          xusoO
 // @author          Fritanga
