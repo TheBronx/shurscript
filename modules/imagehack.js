e767a85e0531e73e3f21d4c14af7e8837c109b4f
rubdev <rubenballester@outlook.com>
2014-08-12 13:31:54 +0200

    ImageHack
    ImageHack correctamente actualizado para su funcionamiento en Master, no
    tiene mucho misterio.

--- modules/imagehack.js
+++ modules/imagehack.js
@@ -1,17 +1,30 @@
+/**
+* ImageHack es un pequeño módulo que adapta el máximo de anchura de la imagen al 99% del visor de tu navegador
+*/
 (function ($, createModule, undefined) {
-	'use strict';
-
+'use strict';
 	var mod = createModule({
 		id: 'imagehack',
-		name: 'Hack imágenes',
+		name: 'Hack para imágenes',
 		author: 'RubDev',
 		version: '0.1',
-		description: 'Permite que las imagenes no desajusten el foro en su totalidad adaptándose a la anchura del navegador.',
+		description: 'Permite adaptar las imágenes excepcionalmente grandes al tamaño máximo del navegador del usuario, sin descuadrar el foro.',
 		domain: 'ALL',
 		initialPreferences: {
-			enabled: true
-		},
-		preferences: {}
+			enabled: true, // Esta es opcional - por defecto true
+
+							}
 	});
-GM.addStyle('img {max-width:99%} ');
+	mod.normalStartCheck = function () {return true;};
+	/**
+	* Funcion a la que se llama en si .normalStartCheck ha dado true
+	*
+	* Principal puerta al modulo. Normalmente toda la logica empieza aqui
+	*/
+	mod.onNormalStart = function () {};
+
+	//MiniHack CSS activado
+
+		addStyle('img {max-width:99%} ');
+
 )(jQuery, SHURSCRIPT.moduleManager.createModule);
