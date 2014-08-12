25bf59892b0a9884e78b941a928b0e207d9738eb
rubdev <rubenballester@outlook.com>
2014-08-12 13:19:40 +0200

    Módulo Post2Pdf
    Commit para la actualización del módulo post2pdf para shurscript en
    versión experimental libre de crashes

--- modules/post2pdf.js
+++ modules/post2pdf.js
@@ -1,36 +1,53 @@
+/**
+* Post2PDF para ShurScript permite exportar cualquier tema de FC a un archivo PDF gracias a las librerías jsPDF
+*/
 (function ($, createModule, undefined) {
-	'use strict';
+'use strict';
+var mod = createModule({
+	id: 'post2pdf',
+	name: 'Post2PDF',
+	author: 'RubDev',
+	version: '0.1',
+	description: 'Permite exportar el contenido de un tema en ForoCoches a PDF para descargarlo posteriormente',
+	domain: ['/showthread.php']
+	initialPreferences: {
+		enabled: true, // Esta es opcional - por defecto true
 
-	var mod = createModule({
-		id: 'Post2Pdf',
-		name: 'Convertir tema a PDF',
-		author: 'RubDev',
-		version: '0.2',
-		description: 'Permite convertir el post en un PDF.',
-		domain: ['/showthread.php'],
-		initialPreferences: {
-			enabled: true
-		},
-		preferences: {}
-	});
+						}
+});
+mod.normalStartCheck = function () {return true;};
+/**
+* Funcion a la que se llama en si .normalStartCheck ha dado true
+*
+* Principal puerta al modulo. Normalmente toda la logica empieza aqui
+*/
+mod.onNormalStart = function () {};
+
+//Inicializamos el módulo jsPDF
 
 var doc = new jsPDF();
-//Creamos un pequeño botón para exportar a PDF
+
+//Creamos un pequeño botón para exportar a PDF justo al lado del contador de citas
+
 $(".page table td.alt2[nowrap]").first().parent().append('<td style="padding: 0px;" class="alt2"><div class="pdfbox">Exportar PDF</div></td>');
+
 $('.pdfbox').click(function () {
-/*Este código lo podemos usar para obviar ciertas secciones del foro que no queremos que salgan en el renderizado del HTML
-// We'll make our own renderer to skip this editor
-var specialElementHandlers = {
-	'#editor': function(element, renderer){
+
+//Excluimos objetos que no interesa exportar
+
+	var specialElementHandlers = {
+		'#div-970x90': function(element, renderer){
 		return true;
-	}
-};
-*/
-//Generamos
-doc.fromHTML($('body').get(0), 15, 15, {
-	'width': 170, 
-	'elementHandlers': specialElementHandlers
-});
-doc.output('dataurlnewwindow');     //Abrimos el PDF en una nueva ventana ya renderizado
+		}
+	};
+
+//Generamos el archivo PDF
+
+	doc.fromHTML($('body').get(0), 15, 15, {
+		'width': 170, 
+		'elementHandlers': specialElementHandlers
+	});
+	
+	doc.output('dataurlnewwindow');     //Abrimos el PDF en una nueva ventana ya renderizado
 }
-)(jQuery, SHURSCRIPT.moduleManager.createModule);
+})(jQuery, SHURSCRIPT.moduleManager.createModule);
