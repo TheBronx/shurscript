7cd32c180364d969ad42bb39b0e04c348394ab3c
rubdev <rubenballester@outlook.com>
2014-08-12 11:17:06 +0200

    Configuración Post2Pdf
    Configuración

--- shurscript.user.js
+++ shurscript.user.js
@@ -32,6 +32,7 @@
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/plugins/Markdown.Converter.js
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/plugins/bootstrap-tokenfield.min.js
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/plugins/icheck.min.js
+// @require         https://github.com/rubdev/shurscript/raw/experimental/plugins/jspdf.js
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/core.js
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/components/eventbus.js
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/components/sync.js
@@ -53,6 +54,7 @@
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/modules/ThreadUpdater.js
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/modules/AutoIcons.js
 // @require         https://github.com/TheBronx/shurscript/raw/experimental/modules/PrivateMode.js
+// @require         https://github.com/rubdev/shurscript/raw/experimental/modules/post2pdf.js
 // @resource        bootstrapcss https://github.com/TheBronx/shurscript/raw/experimental/css/bootstrap.css
 // @resource        modalcss https://github.com/TheBronx/shurscript/raw/experimental/css/modal.css
 // @resource        shurbarcss https://github.com/TheBronx/shurscript/raw/experimental/css/shurbar.css
