7393c1d955309ee4e793e1c81ac8839be6aaa7ca
Fran Exposito <8macau8@gmail.com>
2014-11-26 21:23:43 +0100

    Añadido modulo AutoSpoiler a shurscript.user.js

--- modules/AutoSpoiler.js
+++ modules/AutoSpoiler.js
@@ -30,9 +30,19 @@
     /*Buscamos los post con etiquetas quote y modificamos */
     SHURSCRIPT.eventbus.on('parsePost', parsePost);
     /* Añadimos el botón */
-    /*SHURSCRIPT.eventbus.on('editorReady', function () {
-      addSpoilerButton();
-    });*/
+    SHURSCRIPT.eventbus.on('editorReady', function () {
+      if (!isWYSIWYG()) { //Chrome, Safari, etc.
+        enableWYSIWYG();
+        var checkWYSIWYG = setInterval(function () {
+          if (getEditorBody()) { //WYSIWYG activado
+            clearInterval(checkWYSIWYG);
+            addSpoilerButton();
+          }
+        }, 500);
+      } else { //Firefox
+        addSpoilerButton();
+      }
+    });
   };
 
   /* Pasamos el contenido del post a hideSpoiler */
@@ -81,7 +91,24 @@
     return false;
   }
 
-  /* */
+  function enableWYSIWYG() {
+
+    var editor = getEditor();
+
+    if (!isQuickReply()) {
+      $('#' + editor.editorid + '_textarea').css('width', 600);
+    }
+
+    unsafeWindow.switch_editor_mode(editor.editorid);
+    unsafeWindow.is_saf = false;
+    unsafeWindow.is_moz = true;
+    editor.wysiwyg_mode = 1;
+
+    if ($('#' + editor.editorid + '_cmd_switchmode').length == 0) //Añadimos el boton de cambiar de Editor
+      $('<td><div id="' + editor.editorid + '_cmd_switchmode" class="imagebutton" style="background: none repeat scroll 0% 0% rgb(225, 225, 226); color: rgb(0, 0, 0); padding: 1px; border: medium none;"><img height="20" width="21" alt="Cambiar Modo de Editor" src="http://cdn.forocoches.com/foro/images/editor/switchmode.gif" title="Cambiar Modo de Editor"></div></td>').insertAfter($('#vB_Editor_QR_cmd_resize_0_99').parent());
+  }
+
+  /* Añadimos el boton */
   function addSpoilerButton() {
 
     genericHandler = function (A) {
@@ -125,6 +152,18 @@
 
   function isQuickReply() {
     return unsafeWindow.vB_Editor.vB_Editor_QR !== undefined;
+  }
+
+  function getEditorBody() {
+    return (isQuickReply() ? $("#vB_Editor_QR_iframe") : $("#vB_Editor_001_iframe")).get(0).contentDocument.body;
+  }
+
+  function isWYSIWYG() {
+    try {
+      return getEditorBody();
+    } catch (e) {
+      return false;
+    }
   }
 
 })(jQuery, SHURSCRIPT.moduleManager.createModule);
