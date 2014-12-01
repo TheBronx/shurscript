a7cc6fe470521ee56bfaa714b987e2f853f2265f
Fran Exposito <8macau8@gmail.com>
2014-12-01 17:24:54 +0100

    Modulo Image Gallery

--- modules/ImageGallery.js
+++ modules/ImageGallery.js
@@ -47,7 +47,7 @@
     var modal = '<div id="gl"><div id="gallery" class="modal fade modal-tag" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">'
     + '<div class="modal-dialog modal-lg"><div class="modal-content"><div class="modal-header">'
     + '<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</button>'
-    + '<h3>Galería</h3></div><div class="modal-body"></div><div class="modal-footer"><h4>'+title+'</h4>'
+    + '<h3>Galería</h3><h6>'+images.length+' imágenes</h6></div><div class="modal-body"></div><div class="modal-footer"><h4>'+title+'</h4>'
     + '</div></div></div></div></div>';
 
     $('body').append(modal);
