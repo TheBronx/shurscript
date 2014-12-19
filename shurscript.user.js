79e7621dc0a2949019bf7a60c37a4f207c7cb274
TheBronx <salmarina5@gmail.com>
2014-12-19 20:59:56 +0100

    v0.288.0
    v0.288.0

--- shurscript.user.js
+++ shurscript.user.js
@@ -6,7 +6,7 @@
 // @name            ShurScript
 // @description     Script para ForoCoches
 // @namespace       http://shurscript.es
-// @version         0.25.0-exp
+// @version         0.288.0
 // @author          TheBronx
 // @author          xusO
 // @author          Fritanga / Korrosivo
